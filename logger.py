795ca3c8a2ab3defb7f06ac36d04ba12f47c7a5c
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2026-01-12 19:44:12 +0530

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,13 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    handlers=[
+        logging.FileHandler("bot.log"),
+        logging.StreamHandler()
+    ]
+)
+
+def get_logger(name):
+    return logging.getLogger(name)
