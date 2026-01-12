9107d5a4f3830910126e49faa399ce322ce5a617
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2026-01-12 18:56:33 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+API_ID = 123456
+API_HASH = "your_api_hash_here"
+
+BOT_TOKEN = "your_bot_token_here"
+SESSION_STRING = "your_userbot_session_here"
+
+X_BOT_USERNAME = "xbotusername"     # without @
+Y_GROUP_ID = -1001234567890         # group id where mega link & /status2 goes
